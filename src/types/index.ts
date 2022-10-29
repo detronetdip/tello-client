ceda0e56919b01a025242a21e556211d3dc87ab6
detronetdip <ayondip2001@gmail.com>
2022-10-29 12:03:23 +0530

    added atoms

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,33 @@
 import React from "react";
 
-export interface ButtonProps {
+interface ClassAndStyles {
   Class?: string;
   styles?: object;
+}
+export interface ButtonProps extends ClassAndStyles {
   content: string;
   isDisable?: boolean;
   onclick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
   ripple?: boolean;
+}
+
+export interface InputProps extends ClassAndStyles {
+  placeholder: string;
+  type: string;
+  view?: "INPUT" | "TEXTAREA";
+  value?: string | number;
+  onBlur?: (
+    e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+  onFocus?: (
+    e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+  onChange?: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+}
+
+export interface TooltipProps extends ClassAndStyles {
+  info: string;
+  position: "LEFT" | "RIGHT" | "TOP" | "BOTTOM";
 }
