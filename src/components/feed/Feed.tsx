98900b4c43047715dd3bd690074a12a646110f9f
parijat965 <parijatghosh2@gmail.com>
2022-11-06 15:01:32 +0530

    color changed

--- src/components/feed/Feed.tsx
+++ src/components/feed/Feed.tsx
@@ -1,10 +1,34 @@
 import React from "react";
+import Input from "../atoms/Input";
+import { BiImageAdd } from "react-icons/bi";
+import Button from "../atoms/Button";
 
 const Feed = () => {
   return (
     <>
       <div className="post">
-        <div className="postcrte"></div>
+        <div className="postcrte">
+          <p className="name">Create a Post</p>
+          <hr />
+
+          <div className="post1">
+            <form className="postform">
+              <Input Class="aa" type="text" view="TEXTAREA" placeholder="Express Your Thought" />
+            </form>
+
+            <div className="post2">
+              <div className="imgicon">
+                <BiImageAdd />
+              </div>
+
+              <Button 
+              content="Post" 
+              Class="btn-1" 
+              ripple={true} 
+              />
+            </div>
+          </div>
+        </div>
 
         <div className="mypost"></div>
       </div>
