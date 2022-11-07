3c514b2c2401ead51f3bce2ea900194b8d6db00b
detronetdip <ayondip2001@gmail.com>
2022-11-07 22:28:32 +0530

    restructured

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,33 +5,35 @@ import { HiUserCircle } from "react-icons/hi";
 import { useTheme } from "../../hooks/useTheme";
 
 const Navbar = () => {
-  const {theme} =useTheme();
+  const { theme } = useTheme();
   return (
     <>
       <div className={`${theme}-nav_rapper`}>
-        <div className="left">
-          <div className="logo">
-            <img src="/assets/icons/logo.png" alt="" />
-          </div>
-        </div>
-
-        <div className="right">
-          <div className="serachbox">
-            <div className="srchicon">
-              <RiSearch2Fill />
+        <div className="fixedwrapper">
+          <div className="left">
+            <div className="logo">
+              <img src="/assets/icons/logo.png" alt="" />
             </div>
-            <form>
-              <Input Class="aa" type="text" placeholder="Search Tello" />
-            </form>
           </div>
 
-          <div className="ll">
-            <div className="profileicon">
-              <HiUserCircle />
+          <div className="right">
+            <div className="serachbox">
+              <div className="srchicon">
+                <RiSearch2Fill />
+              </div>
+              <form>
+                <Input Class="aa" type="text" placeholder="Search Tello" />
+              </form>
             </div>
 
-            <div className="username">
-              <p>User_name</p>
+            <div className="ll">
+              <div className="profileicon">
+                <HiUserCircle />
+              </div>
+
+              <div className="username">
+                <p>User_name</p>
+              </div>
             </div>
           </div>
         </div>
