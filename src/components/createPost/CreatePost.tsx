5bb372c5db8a6b2a4a3add649c55924d8171e890
detronetdip <ayondip2001@gmail.com>
2022-11-07 22:58:00 +0530

    create post component added

---
+++ src/components/createPost/CreatePost.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { BiImageAdd } from "react-icons/bi";
+import Button from "../atoms/Button";
+import Input from "../atoms/Input";
+
+const CreatePost = () => {
+  return (
+    <>
+      <div className="postcrte">
+        <p className="name">Create a Post</p>
+        <hr />
+
+        <div className="post1">
+          <form className="postform">
+            <Input
+              Class="aa"
+              type="text"
+              view="TEXTAREA"
+              placeholder="Express Your Thought"
+            />
+          </form>
+
+          <div className="post2">
+            <div className="imgicon">
+              <BiImageAdd />
+            </div>
+
+            <Button content="Post" Class="btn-1" ripple={true} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CreatePost;
--- src/components/feed/Feed.tsx
+++ src/components/feed/Feed.tsx
@@ -1,34 +1,11 @@
 import React from "react";
-import Input from "../atoms/Input";
-import { BiImageAdd } from "react-icons/bi";
-import Button from "../atoms/Button";
+import CreatePost from "../createPost/CreatePost";
 
 const Feed = () => {
   return (
     <>
       <div className="post">
-        <div className="postcrte">
-          <p className="name">Create a Post</p>
-          <hr />
-
-          <div className="post1">
-            <form className="postform">
-              <Input Class="aa" type="text" view="TEXTAREA" placeholder="Express Your Thought" />
-            </form>
-
-            <div className="post2">
-              <div className="imgicon">
-                <BiImageAdd />
-              </div>
-
-              <Button 
-              content="Post" 
-              Class="btn-1" 
-              ripple={true} 
-              />
-            </div>
-          </div>
-        </div>
+        <CreatePost />
 
         <div className="mypost"></div>
       </div>
