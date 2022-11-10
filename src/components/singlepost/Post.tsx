47450ad81c0b58d3cdc21249264aadd4719a59df
parijat965 <parijatghosh2@gmail.com>
2022-11-11 00:28:53 +0530

    moment import

--- src/components/singlepost/Post.tsx
+++ src/components/singlepost/Post.tsx
@@ -1,4 +1,4 @@
-import moment from 'moment'
+import * as moment from 'moment'
 import React from 'react'
 import { AiOutlineHeart } from 'react-icons/ai'
 import { BiDotsVerticalRounded } from 'react-icons/bi'
