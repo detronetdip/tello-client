1c3fe8afde30d84bffd240fee85f4dc392cce9ad
detronetdip <ayondip2001@gmail.com>
2022-12-17 20:15:28 +0530

    add click menu to the post

--- src/components/singlepost/Post.tsx
+++ src/components/singlepost/Post.tsx
@@ -1,65 +1,86 @@
-import moment from 'moment'
-import React from 'react'
-import { AiOutlineHeart } from 'react-icons/ai'
-import { BiDotsVerticalRounded } from 'react-icons/bi'
-import { BsBookmarkHeartFill } from 'react-icons/bs'
-import { MdComment } from 'react-icons/md'
-import { useTheme } from '../../hooks/useTheme'
+import moment from "moment";
+import React, { useEffect, useRef, useState } from "react";
+import { AiOutlineHeart } from "react-icons/ai";
+import { BiDotsVerticalRounded } from "react-icons/bi";
+import { BsBookmarkHeartFill } from "react-icons/bs";
+import { MdComment } from "react-icons/md";
+import { useTheme } from "../../hooks/useTheme";
 
-function Post () {
-    const {theme} = useTheme();
+function Post() {
+  const { theme } = useTheme();
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+  const handelMenu = (event: any) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setOpenMenu(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("mousedown", handelMenu);
+
+    return () => {
+      window.removeEventListener("mousedown", handelMenu);
+    };
+  }, []);
+
   return (
-  
-       <div className={`${theme}-postwrapper`}>
-       <div className="mypost">
-          <div className="head">
-            <div className="aa">
-              <div className="imgicon">
-                <img src="/assets/icons/fakeuser.jpg" alt="" />
-              </div>
-
-              <div className="username">
-                <p>User_name</p>
-                <p className="date">{moment().format("MMM Do YY")}</p>
-              </div>
+    <div className={`${theme}-postwrapper`}>
+      <div className="mypost">
+        <div className="head">
+          <div className="aa">
+            <div className="imgicon">
+              <img src="/assets/icons/fakeuser.jpg" alt="" />
             </div>
 
-            <div className="dropdown">
-              <BiDotsVerticalRounded />
-
-              <div className="dropmenu"></div>
+            <div className="username">
+              <p>User_name</p>
+              <p className="date">{moment().format("MMM Do YY")}</p>
             </div>
           </div>
 
-          <hr className="hr2" />
-
-          <div className="postimg">
-            <div>
-              <img
-                src="https://osnabruegge.github.io/images/demo/demo-landscape.jpg"
-                alt="slow internet...."
-              />
-            </div>
-          </div>
-
-          <div className="option1">
-            <div className="aa">
-              <div className="like">
-                <AiOutlineHeart />
+          <div className="dropdown">
+            <BiDotsVerticalRounded onClick={() => setOpenMenu(!openMenu)} />
+            {openMenu ? (
+              <div className="dropmenu" ref={menuRef}>
+                <ul>
+                  <li>Copy link</li>
+                  <li>View profile</li>
+                  <li>Add to favourites</li>
+                  <li>Send</li>
+                </ul>
               </div>
-              &nbsp; &nbsp;
-              <div className="cmnt">
-                <MdComment />
-              </div>
-            </div>
-            <div className="bookmark">
-              <BsBookmarkHeartFill />
-            </div>
+            ) : null}
           </div>
         </div>
-       </div>
-    
-  )
+
+        <hr className="hr2" />
+
+        <div className="postimg">
+          <div>
+            <img
+              src="https://osnabruegge.github.io/images/demo/demo-landscape.jpg"
+              alt="slow internet...."
+            />
+          </div>
+        </div>
+
+        <div className="option1">
+          <div className="aa">
+            <div className="like">
+              <AiOutlineHeart />
+            </div>
+            &nbsp; &nbsp;
+            <div className="cmnt">
+              <MdComment />
+            </div>
+          </div>
+          <div className="bookmark">
+            <BsBookmarkHeartFill />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Post
+export default Post;
