722fe6204d2920f138e95834a48a1164d7bd8615
detronetdip <ayondip2001@gmail.com>
2023-01-03 17:44:40 +0530

    working on responsiveness

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import { BiLogOutCircle } from "react-icons/bi";
 import { BsFillMoonFill, BsSunFill } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
 import Togglebutton from "../components/atoms/Togglebutton";
+import { sidebar } from "../context";
 import { useTheme } from "../hooks/useTheme";
 import { navMenu } from "./data/navMenuData";
 
@@ -16,59 +18,65 @@ function NavMenu() {
       setCurrentTheme("DARK");
     }
   };
+  const sideBar=useRecoilValue(sidebar);
   return (
-    <div className="left1">
-      <div className="fullheight">
-        <div className="option">
-          <ul>
-            {navMenu.map((e) => (
-              <li
-                className="option1"
-                onClick={() => location(e.path)}
-                key={`${e.path}-t`}
-              >
+    <>
+      <div className="left1">
+        <div className="fullheight">
+          <div className="option">
+            <ul>
+              {navMenu.map((e) => (
+                <li
+                  className="option1"
+                  onClick={() => location(e.path)}
+                  key={`${e.path}-t`}
+                >
+                  <div className="icon">
+                    <e.icon />
+                  </div>
+                  <div className="name">
+                    <p>{e.title}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="modechange">
+            <ul>
+              <li className="option2" onClick={() => location("/auth")}>
                 <div className="icon">
-                  <e.icon />
+                  <BiLogOutCircle />
                 </div>
                 <div className="name">
-                  <p>{e.title}</p>
+                  <p>Sign out</p>
                 </div>
               </li>
-            ))}
-          </ul>
-        </div>
+              <li className="option2" onClick={toggleTheme}>
+                <div className="icon">
+                  {theme === "LIGHT" ? <BsFillMoonFill /> : <BsSunFill />}
+                </div>
+              </li>
+            </ul>
 
-        <div className="modechange">
-          <ul>
-            <li className="option2" onClick={() => location("/auth")}>
+            <div className="mode">
               <div className="icon">
-                <BiLogOutCircle />
+                <BsSunFill />
               </div>
-              <div className="name">
-                <p>Sign out</p>
+              <div className="toggle">
+                <Togglebutton label="theme" />
               </div>
-            </li>
-            <li className="option2" onClick={toggleTheme}>
               <div className="icon">
-                {theme === "LIGHT" ? <BsFillMoonFill /> : <BsSunFill />}
+                <BsFillMoonFill />
               </div>
-            </li>
-          </ul>
-
-          <div className="mode">
-            <div className="icon">
-              <BsSunFill />
-            </div>
-            <div className="toggle">
-              <Togglebutton label="theme" />
-            </div>
-            <div className="icon">
-              <BsFillMoonFill />
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className={`smallsidebar ${sideBar.open?`smallsidebar-expand`:`smallsidebar-close`}`}>
+        m
+      </div>
+    </>
   );
 }
 
