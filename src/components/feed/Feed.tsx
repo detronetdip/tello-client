64f89a559cc1f7f68a4b867584f044f4b0fd7be6
detronetdip <ayondip2001@gmail.com>
2023-05-16 11:09:20 +0530

    news feed fetching

--- src/components/feed/Feed.tsx
+++ src/components/feed/Feed.tsx
@@ -1,17 +1,36 @@
 import CreatePost from "../createPost/CreatePost";
+import { gql, useQuery } from "@apollo/client";
+import { useRecoilValue } from "recoil";
+import { userState } from "../../context";
+import { useEffect } from "react";
 
-function Feed () {
+function Feed() {
+  const { userId } = useRecoilValue(userState);
+  const GET_MY_FEED = gql`
+    query myFeed($uid: String) {
+      myposts(uid: $uid) {
+        id
+      }
+    }
+  `;
+  const { loading, error, data, refetch } = useQuery(GET_MY_FEED, {
+    variables: {
+      uid: userId,
+    },
+  });
+
+  useEffect(() => {
+    refetch({ uid: userId });
+    console.log(data);
+  }, []);
+
   return (
-    
-      <div className="post">
-        <CreatePost />
+    <div className="post">
+      <CreatePost />
 
-        <hr className="hr1" />
-
-        
-      </div>
-    
+      <hr className="hr1" />
+    </div>
   );
-};
+}
 
 export default Feed;
